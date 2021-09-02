ca00433839523106a2c8057c63833b9acd7dab3e
metmatera <74006870+metmatera@users.noreply.github.com>
2021-09-02 14:51:30 +0200

    Update networks.py

--- Global/models/networks.py
+++ Global/models/networks.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import functools
-from torch.autograd import Variable
 import numpy as np
+
+from torch.autograd import Variable
 from torch.nn.utils import spectral_norm
