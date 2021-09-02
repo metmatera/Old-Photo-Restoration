ce6c70016b9f7dc82b92f5240e9d7a318ba13abc
metmatera <74006870+metmatera@users.noreply.github.com>
2021-09-02 15:13:10 +0200

    Update networks.py

--- Global/models/networks.py
+++ Global/models/networks.py
@@ -6,3 +6,15 @@ import numpy as np
 
 from torch.autograd import Variable
 from torch.nn.utils import spectral_norm
+
+class Encoder(nn.Module):
+  def __init__(self, input_nc, output_nc, ngf=32, n_downsampling=4, norm_layer=nn.BatchNorm2d):
+    super(Encoder, self).__init__()
+    self.output_nc = output_nc
+    
+    model = [
+      nn.ReflectionPad2d(3),
+      nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0),
+      norm_layer(ngf),
+      nn.ReLU(True)
+    ]
